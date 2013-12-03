583e40987c29dc4349f436e897f8752a58c95ece
hwg <hwg@rock-chips.com>
2013-12-03 15:15:55 +0800

    wifi: support mtk wifi bt

--- hostapd/Android.mk
+++ hostapd/Android.mk
@@ -24,6 +24,10 @@ L_CFLAGS += -DVERSION_STR_POSTFIX=\"-$(PLATFORM_VERSION)\"
 # Set Android log name
 L_CFLAGS += -DANDROID_LOG_NAME=\"hostapd\"
 
+ifeq ($(BOARD_WLAN_DEVICE), mediatek)
+L_CFLAGS += -DANDROID_P2P
+endif
+
 ifeq ($(BOARD_WLAN_DEVICE), bcmdhd)
 L_CFLAGS += -DANDROID_P2P
 endif
